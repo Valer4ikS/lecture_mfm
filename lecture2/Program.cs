10184854edca7734d5a2d832718b496e8160ece1
Valer4ikS <maraclee@yandex.ru>
2023-02-20 05:55:14 +0300

    fix?

--- lecture2/Program.cs
+++ lecture2/Program.cs
@@ -7,25 +7,23 @@
 //     return result; // возвращаемое значение
 // }
 
- 
+ // int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
 
-int Max(int arg1, int arg2, int arg3)
-{
-    int result = arg1;
-    if (arg2 > result) result = arg2;
-    if (arg3 > result) result = arg3;
-    return result;
-}
-
-int a1 = 15;
-int b1 = 21;
-int c1 = 3229;
-int a2 = 12;
-int b2 = 21212;
-int c2 = 33;
-int a3 = 13;
-int b3 = 23;
-int c3 = 313;
+// int a1 = 15;
+// int b1 = 21;
+// int c1 = 3229;
+// int a2 = 12;
+// int b2 = 21212;
+// int c2 = 33;
+// int a3 = 13;
+// int b3 = 23;
+// int c3 = 313;
 
 // int max = a1;
 // if (b1 > max) max = b1;
@@ -44,6 +42,28 @@ int c3 = 313;
 // int max = Max(max1, max2, max3);
 // тоже самое, что int max ниже
 
-int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+// int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+// Console.WriteLine(max);
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+            //  0
+int[] array = {14, 26, 33, 45, 76, 88, 12, 15, 22};
+array[0] = 12;
+
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
 
 Console.WriteLine(max);
+
+// 11
---
+++ lecture2_2/Program.cs
@@ -0,0 +1,74 @@
+﻿// Одномерный массив array из n элементов, требуется найти элемент массива, равный find
+// 1. Установить счетчик index в позицию 0
+// 2. Если array[index] = find, алгоритм завершил работу успешно
+// 3. Увеличить index на 1
+// 4. Если index < n, то перейти к шагу 2. В противном случае алгоритм завершил работу безуспешно
+
+// int[] array = { 12, 15, 44, 15, 12, 18, 52 };
+// int n = array.Length;
+// int find = 18;
+// int index = 0;
+
+// while (index < n)
+// {
+//     if (array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     index++; // index = index + 1
+// }
+
+
+// второй пример с функциями void - ничего не возвращают
+
+void FillArray(int[] collection)  // заполняем new int[10] рандомными числами
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) // метод который печатает массив
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find) // возвращает позицию, индекс
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+     
+
+int[] array = new int[10]; // первое - определяем массив, по умолчанию заполнен 0, для этого Fillarray.
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
+
